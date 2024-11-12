0bb4f6c693fe6de0bab35ebcc7cdfa38578642af
Morning Zeng <zengchennihon@gmail.com>
2024-11-12 13:46:40 +0800

    Refactor actions into separate methods
    Grouped the actions "softWrapAction", "scrollToEndAction", and "clearAllAction" into their own methods for better readability and maintainability. This improves the structure of the code by isolating logic for each action separately.

--- src/main/java/com/github/morningzeng/toolset/component/LanguageTextArea.java
+++ src/main/java/com/github/morningzeng/toolset/component/LanguageTextArea.java
@@ -252,30 +252,38 @@ public final class LanguageTextArea extends LanguageTextField {
     }
 
     public AnAction[] defaultRightBarActions() {
-        return new AnAction[]{
-                new AnAction(Actions.ToggleSoftWrap) {
-                    @Override
-                    public void actionPerformed(@NotNull final AnActionEvent e) {
-                        final EditorSettings settings = editor.getSettings();
-                        settings.setUseSoftWraps(!settings.isUseSoftWraps());
-                    }
-                },
-                new AnAction(RunConfigurations.Scroll_down) {
-                    @Override
-                    public void actionPerformed(@NotNull final AnActionEvent e) {
-                        final DocumentEx document = editor.getDocument();
-                        final int lastLine = document.getLineCount() - 1;
-                        final int lastLineStartOffset = document.getLineStartOffset(lastLine);
-                        editor.getCaretModel().moveToOffset(lastLineStartOffset);
-                        editor.getScrollingModel().scrollToCaret(ScrollType.MAKE_VISIBLE);
-                    }
-                },
-                new AnAction(Actions.GC) {
-                    @Override
-                    public void actionPerformed(@NotNull final AnActionEvent e) {
-                        setText("");
-                    }
-                }
+        return new AnAction[]{this.softWrapAction(), this.scrollToEndAction(), this.clearAllAction()};
+    }
+
+    private @NotNull AnAction softWrapAction() {
+        return new AnAction("Soft-Wrap", "Soft-Wrap", Actions.ToggleSoftWrap) {
+            @Override
+            public void actionPerformed(@NotNull final AnActionEvent e) {
+                final EditorSettings settings = editor.getSettings();
+                settings.setUseSoftWraps(!settings.isUseSoftWraps());
+            }
+        };
+    }
+
+    private @NotNull AnAction scrollToEndAction() {
+        return new AnAction("Scroll to End", "Scroll to End", RunConfigurations.Scroll_down) {
+            @Override
+            public void actionPerformed(@NotNull final AnActionEvent e) {
+                final DocumentEx document = editor.getDocument();
+                final int lastLine = document.getLineCount() - 1;
+                final int lastLineStartOffset = document.getLineStartOffset(lastLine);
+                editor.getCaretModel().moveToOffset(lastLineStartOffset);
+                editor.getScrollingModel().scrollToCaret(ScrollType.MAKE_VISIBLE);
+            }
+        };
+    }
+
+    private @NotNull AnAction clearAllAction() {
+        return new AnAction("Clear All", "Clear all", Actions.GC) {
+            @Override
+            public void actionPerformed(@NotNull final AnActionEvent e) {
+                setText("");
+            }
         };
     }
 
